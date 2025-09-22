358ed68bf2e44568d191c6d4f2c1ff026e2ab216
Ali Hamza <ali.mhamza06@gmail.com>
2025-09-23 00:29:28 +0400

    Clean up and extend Object.cpp.

--- src/Object.cpp
+++ src/Object.cpp
@@ -1,32 +1,9 @@
 #include "../include/Object.h"
+#include "../include/LoxClass.h"
 #include "../include/LoxFunction.h"
 #include "../include/Types.h"
 #include <string>
 #include <typeinfo>
-
-std::string Object::printVal()
-{
-	if (type(*this) == NUM)
-		return std::to_string(any_cast<double>(this->value));
-	if (type(*this) == BOOL)
-	{
-		bool v = any_cast<bool>(this->value);
-		if (v) return "true";
-		else
-			return "false";
-	}
-	if (type(*this) == STR)
-		return any_cast<std::string>(this->value);
-    if (type(*this) == LOXFUNC)
-    {
-        LoxFunction function = std::any_cast<LoxFunction>(this->value);
-        return function.declaration.name.lexeme;
-    }
-    if (type(*this) == NONE)
-        return "nil";
-    
-    return "NULL";
-}
 
 std::string Object::printType()
 {
@@ -36,8 +13,12 @@ std::string Object::printType()
         return "boolean";
 	if (type(*this) == STR)
 		return "string";
-    if (type(*this) == LOXFUNC)
+    if (type(*this) == LOX_FUNC)
         return "function";
+    if (type(*this) == LOX_CLASS)
+        return "class";
+    if (type(*this) == LOX_INST)
+        return "class instance";
     if (type(*this) == NONE)
         return "nil";
     
